51fff3916d87416b321c2d662e1ee5b2fd81e7d6
Phil Splettstoeszer <stoeszer09@gmail.com>
2022-05-04 18:50:44 -0500

    Add sequelize

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+
+// SEQUELIZE CONNECTION
+
+let sequelize = new Sequelize({  
+    username: "postgres",
+    password: process.env.password,
+    host: "127.0.0.1",
+    dialect: "postgres"})
+
+// const sequelize = new Sequelize(process.env.PG_URI)
+
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
 
 // ROOT
 app.get('/', (req, res) => {
